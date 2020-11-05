4e8ed4e22ca1266f0d7d8ae73cfc16cc5121fb02
Irvin Natt <ipnatt@gmail.com>
2020-11-05 10:16:43 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,5 +1,6 @@
 def square_array(array)
   counter = 0
+  array[].sqrt
   
   while counter < array.length do
     puts array[counter]
@@ -7,4 +8,3 @@ def square_array(array)
   end
 end
 
-square_array[array]
