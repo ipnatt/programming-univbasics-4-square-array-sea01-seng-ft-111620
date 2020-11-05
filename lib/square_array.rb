440f97d4397db74e87aed8ed01639b5cc2755548
Irvin Natt <ipnatt@gmail.com>
2020-11-05 10:28:41 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,9 @@
 def square_array(array)
   new_array = []
   counter = 0
+  
   while counter < array.length()
-  new_array.push(array[counter] * array[counter])
-  counter += 1
+    new_array.push(array[counter] * array[counter])
+    counter += 1
+  end
   
