23144b4335c00c6a02f357712353482b37be8894
Irvin Natt <ipnatt@gmail.com>
2020-11-05 10:22:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   squared_array = []
-  array.each do |number| 
-    square = number ** 2
+  array.each do |numbers| 
+    square = numbers ** 2
     squared_array << square
   end
   squared_array
